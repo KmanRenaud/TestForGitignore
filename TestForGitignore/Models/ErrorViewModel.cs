23b520cc35c61d4d8b99f8531948bdfd4a0e597a
Kael Renaud <Kmans2272@gmail.com>
2022-10-26 10:31:38 -0500

    Add project files.

---
+++ TestForGitignore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestForGitignore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
